82662bde90b09226c1f90524b6bbd87ec412330b
demar01 <maria.dermit@qmul.ac.uk>
2021-01-14 22:53:50 +0000

    adding test for MapProt2Ensg

--- R/MapProt2Ensg.R
+++ R/MapProt2Ensg.R
@@ -2,6 +2,9 @@
 #'
 #' @param protein_list list
 #' list of proteins 
+#' @param ProtFeatures list
+#' list of annotations. The array component of this list (ProtFeatures$GeneName)
+#' is used in this function.
 #' @examples
 #' if(interactive()){
 #' ENSGid_who = MapProt2Ensg(ProtIDs_who)
@@ -9,7 +12,7 @@
 #' @return a list of mapped genes
 #' 
 #' @export
-MapProt2Ensg <- function(protein_list){
+MapProt2Ensg <- function(protein_list,ProtFeatures=ProtFeatures){
   lapply(protein_list,function(x)
   {y=unname(ProtFeatures$GeneName[x])
   unique(y[!is.na(y)])})
